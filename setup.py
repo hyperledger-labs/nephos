fcf619483eb032b7fc11deb480a9b70fed606136
Alejandro Vicente Grabovetsky <sasha@aid.technology>
2019-01-14 19:27:30 +0200

    [Multi-MSP] Add MANIFEST.in file
    Signed-off-by: Alejandro Vicente Grabovetsky <sasha@aid.technology>

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ URL = 'https://github.com/aidtechnology/nephos'
 EMAIL = 'sasha@aid.technology'
 AUTHOR = 'Alejandro (Sasha) Vicente Grabovetsky'
 REQUIRES_PYTHON = '>=3.7.0'
-VERSION = '0.1.1'
+VERSION = '0.1.3'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
