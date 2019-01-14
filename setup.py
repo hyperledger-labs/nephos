a553375c7707bd71d882d50f8338f6af8fb2b83a
Alejandro Vicente Grabovetsky <sasha@aid.technology>
2019-01-14 19:06:27 +0200

    [Multi-MSP] Bump nephos version up
    Signed-off-by: Alejandro Vicente Grabovetsky <sasha@aid.technology>

--- setup.py
+++ setup.py
@@ -2,6 +2,8 @@
 # -*- coding: utf-8 -*-
 # pragma: no cover
 
+# Acknowledgement: adapted from https://github.com/kennethreitz/setup.py
+
 # Note: To use the 'upload' functionality of this file, you must:
 #   $ pip install twine
 
@@ -19,7 +21,7 @@ URL = 'https://github.com/aidtechnology/nephos'
 EMAIL = 'sasha@aid.technology'
 AUTHOR = 'Alejandro (Sasha) Vicente Grabovetsky'
 REQUIRES_PYTHON = '>=3.7.0'
-VERSION = '0.1.0'
+VERSION = '0.1.1'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
