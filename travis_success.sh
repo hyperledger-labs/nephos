f9dcb9aa34b90b4c6ac8d2768b7b33d6534f2fcb
Alejandro Vicente Grabovetsky <sasha@aid.technology>
2019-01-24 18:05:38 +0200

    [PyPI] Automatically upload to PyPI (and then perform mutation testing)
    Signed-off-by: Alejandro Vicente Grabovetsky <sasha@aid.technology>

---
+++ travis_success.sh
@@ -0,0 +1,21 @@
+codecov --token=$CODECOV_TOKEN
+
+NEPHOS_VERSION=$(cat setup.py | grep 'VERSION =' | awk '{print $3}')
+echo "Nephos version is $NEPHOS_VERSION"
+
+PACKAGE_PYPI=$(curl -s https://pypi.org/pypi/nephos/json | jq '.releases | keys[]' | grep $NEPHOS_VERSION)
+echo "On PyPI we have $PACKAGE_PYPI"
+
+if [ $PACKAGE_PYPI ]
+then
+    echo "Package has already been uploaded to PyPI"
+else
+    python setup.py upload
+    # Cosmic Ray (Mutation testing)
+    pip install cosmic_ray
+    cosmic-ray -v INFO init cosmic_ray_config.yaml my_session.sqlite
+    cosmic-ray -v INFO exec my_session.sqlite
+    cr-report my_session.sqlite | grep 'complete:'
+    cr-report my_session.sqlite | grep 'survival rate:'
+fi
+
