52ecd9336f127acbf2824b5b5ee77ae6796840ee
Alejandro Vicente Grabovetsky <sasha@aid.technology>
2019-01-25 02:26:25 +0200

    0.1.9
    Signed-off-by: Alejandro Vicente Grabovetsky <sasha@aid.technology>

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ URL = "https://github.com/aidtechnology/nephos"
 EMAIL = "sasha@aid.technology"
 AUTHOR = "Alejandro (Sasha) Vicente Grabovetsky"
 REQUIRES_PYTHON = ">=3.7.0"
-VERSION = "0.1.8"
+VERSION = "0.1.9"
 
 # What packages are required for this module to be executed?
 REQUIRED = ["blessings", "click", "kubernetes", "pygments"]
