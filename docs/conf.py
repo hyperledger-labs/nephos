adf2dd098fc962016379356116e7fd9b6440d6d3
Alejandro Vicente Grabovetsky <sasha@aid.technology>
2019-02-21 19:46:30 +0200

    0.2.3
    Signed-off-by: Alejandro Vicente Grabovetsky <sasha@aid.technology>

--- docs/conf.py
+++ docs/conf.py
@@ -27,7 +27,7 @@ author = "Alejandro (Sasha) Vicente Grabovetsky"
 # The short X.Y version
 version = "0.2"
 # The full version, including alpha/beta/rc tags
-release = "0.2.2"
+release = "0.2.3"
 
 
 # -- General configuration ---------------------------------------------------
