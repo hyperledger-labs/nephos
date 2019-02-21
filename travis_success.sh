bc68351b0671cb22f1644ba0f1f6bdc7857be319
Alejandro Vicente Grabovetsky <sasha@aid.technology>
2019-02-21 17:12:24 +0200

    [CryptoFolder] Attempt to fix travis_success.sh to avoid cosmic ray on every commit
    Signed-off-by: Alejandro Vicente Grabovetsky <sasha@aid.technology>

--- travis_success.sh
+++ travis_success.sh
@@ -1,19 +1,22 @@
-codecov --token=$CODECOV_TOKEN
+#!/usr/bin/env bash
+
+codecov --token=${CODECOV_TOKEN}
 
 NEPHOS_VERSION=$(cat setup.py | grep 'VERSION =' | awk '{print $3}')
 echo "Nephos version is $NEPHOS_VERSION"
 
-PACKAGE_PYPI=$(curl -s https://pypi.org/pypi/nephos/json | jq '.releases | keys[]' | grep $NEPHOS_VERSION)
+PACKAGE_PYPI=$(curl -s https://pypi.org/pypi/nephos/json | jq '.releases | keys[]' | grep ${NEPHOS_VERSION})
 echo "On PyPI we have $PACKAGE_PYPI"
 
-if [ $PACKAGE_PYPI ]
+if [[ ${PACKAGE_PYPI} ]]
 then
     echo "Package has already been uploaded to PyPI"
 else
     python setup.py upload
 fi
 
-if [ $TRAVIS_PULL_REQUEST ]
+echo "TRAVIS_PULL_REQUEST is $TRAVIS_PULL_REQUEST"
+if [[ ${TRAVIS_PULL_REQUEST} ]]
 then
     # Cosmic Ray (Mutation testing)
     pip install cosmic_ray
