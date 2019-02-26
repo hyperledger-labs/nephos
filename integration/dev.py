b8714f1b40e9b0208e4a04f7d68ad61cef80dd17
Alejandro Vicente Grabovetsky <sasha@aid.technology>
2019-02-26 19:28:08 +0200

    [DO-373] Cleanup after integration testing
    Signed-off-by: Alejandro Vicente Grabovetsky <sasha@aid.technology>

--- integration/dev.py
+++ integration/dev.py
@@ -1,10 +1,32 @@
 import os
 
-from nephos.fabric.settings import load_config
+from nephos.fabric.settings import load_config, check_cluster
+from nephos.helpers.misc import execute
 from nephos.runners import runner_fabric
 
 CURRENT_PATH = os.path.abspath(os.path.split(__file__)[0])
+# We will check cluster and flatly refuse to do integration testing unless on 'minikube'
+CONTEXT = 'minikube'
 
-
+# Get options
 opts = load_config(os.path.join(CURRENT_PATH, '..', 'examples', 'dev', 'nephos_config.yaml'))
+
+# TODO: There should be a more elegant way of obtaining all the releases
+releases = (
+    [key for key in opts['cas'].keys()] +
+    opts['orderers']['names'] +
+    [('cdb-' + key) for key in opts['peers']['names']] +
+    [key for key in opts['peers']['names']]
+)
+
+# Run Fabric script
+check_cluster(CONTEXT)  # Dangerous operation, recheck we have not shifted context
 runner_fabric(opts, upgrade=False, verbose=False)
+
+# Delete all deployments from Helm
+check_cluster(CONTEXT)  # Dangerous operation, recheck we have not shifted context
+execute('helm delete --purge {}'.format(' '.join(releases)))
+
+# Delete the namespaces
+check_cluster(CONTEXT)  # Dangerous operation, recheck we have not shifted context
+execute('kubectl delete ns orderers peers'.format(' '.join(releases)))
