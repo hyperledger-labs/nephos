e2927c68ce99b2d9472e65b489f8de185df9e3a2
Alejandro Vicente Grabovetsky <sasha@aid.technology>
2019-02-26 19:36:27 +0200

    [DO-373] Refactor dev integration test into pytest test
    Signed-off-by: Alejandro Vicente Grabovetsky <sasha@aid.technology>

---
+++ integration/test_dev.py
@@ -0,0 +1,45 @@
+import os
+
+from click.testing import CliRunner
+
+from nephos.deploy import cli
+
+RUNNER = CliRunner()
+
+from nephos.fabric.settings import load_config, check_cluster
+from nephos.helpers.misc import execute
+
+CURRENT_PATH = os.path.abspath(os.path.split(__file__)[0])
+
+
+class TestIntegrationDev:
+    # We will check cluster and flatly refuse to do integration testing unless on 'minikube'
+    CONTEXT = 'minikube'
+    CONFIG = os.path.join(CURRENT_PATH, '..', 'examples', 'dev', 'nephos_config.yaml')
+
+    def test_integration_dev(self):
+        # Get options
+        opts = load_config(self.CONFIG)
+
+        # TODO: There should be a more elegant way of obtaining all the releases
+        releases = (
+            [key for key in opts['cas'].keys()] +
+            opts['orderers']['names'] +
+            [('cdb-' + key) for key in opts['peers']['names']] +
+            [key for key in opts['peers']['names']]
+        )
+
+        # Run Fabric script
+        check_cluster(self.CONTEXT)  # Dangerous operation, recheck we have not shifted context
+        result = RUNNER.invoke(cli, ["--settings_file", self.CONFIG, "fabric"])
+
+        # Delete all deployments from Helm
+        check_cluster(self.CONTEXT)  # Dangerous operation, recheck we have not shifted context
+        execute('helm delete --purge {}'.format(' '.join(releases)))
+
+        # Delete the namespaces
+        check_cluster(self.CONTEXT)  # Dangerous operation, recheck we have not shifted context
+        execute('kubectl delete ns orderers peers'.format(' '.join(releases)))
+
+        # Finally assert everything ran correctly
+        assert result.exit_code == 0
