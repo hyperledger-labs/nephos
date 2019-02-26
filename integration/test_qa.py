9091a9b369b2403e71728271b8f52031d41a3ad6
Alejandro Vicente Grabovetsky <sasha@aid.technology>
2019-02-26 20:25:00 +0100

    [DO-373] Integration test for QA example
    Signed-off-by: Alejandro Vicente Grabovetsky <sasha@aid.technology>

---
+++ integration/test_qa.py
@@ -0,0 +1,56 @@
+import os
+
+from click.testing import CliRunner
+
+from nephos.deploy import cli
+
+RUNNER = CliRunner()
+
+from nephos.fabric.settings import load_config, check_cluster
+from nephos.helpers.k8s import ns_create
+from nephos.helpers.misc import execute
+
+CURRENT_PATH = os.path.abspath(os.path.split(__file__)[0])
+
+
+class TestIntegrationQa:
+    # We will check cluster and flatly refuse to do integration testing unless on 'minikube'
+    CONTEXT = 'minikube'
+    CONFIG = os.path.join(CURRENT_PATH, '..', 'examples', 'qa', 'nephos_config.yaml')
+    TLS_PATH = os.path.join(CURRENT_PATH, '..', 'examples', 'ca-nephos-local')
+
+    def test_integration_qa(self):
+        # Get options
+        opts = load_config(self.CONFIG)
+
+        # Save TLS of each CA in its relevant secret
+        ns_create('cas')
+
+        # TODO: Eventually we should enable getting path for multiple CAs programatically
+        execute(("kubectl -n cas create secret tls ca--tls " +
+                 "--cert={tls_path}.crt " +
+                 "--key={tls_path}.key"
+                 ).format(tls_path=self.TLS_PATH))
+
+        # TODO: There should be a more elegant way of obtaining all the releases
+        releases = (
+            [key for key in opts['cas'].keys()] +
+            opts['orderers']['names'] +
+            [('cdb-' + key) for key in opts['peers']['names']] +
+            [key for key in opts['peers']['names']]
+        )
+
+        # Run Fabric script
+        check_cluster(self.CONTEXT)  # Dangerous operation, recheck we have not shifted context
+        result = RUNNER.invoke(cli, ["--settings_file", self.CONFIG, "fabric"])
+
+        # Delete all deployments from Helm
+        check_cluster(self.CONTEXT)  # Dangerous operation, recheck we have not shifted context
+        execute('helm delete --purge {}'.format(' '.join(releases)))
+
+        # Delete the namespaces
+        check_cluster(self.CONTEXT)  # Dangerous operation, recheck we have not shifted context
+        execute('kubectl delete ns cas orderers peers'.format(' '.join(releases)))
+
+        # Finally assert everything ran correctly
+        assert result.exit_code == 0
