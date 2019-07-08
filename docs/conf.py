423b724e8f80fdb0fd689ed946d0522ab7561585
Inzamam <inzamam.15@cse.mrt.ac.lk>
2019-07-08 18:30:38 +0530

    upgraded the version
    Signed-off-by: Inzamam <inzamam.15@cse.mrt.ac.lk>

--- docs/conf.py
+++ docs/conf.py
@@ -27,7 +27,7 @@ author = "Alejandro (Sasha) Vicente Grabovetsky"
 # The short X.Y version
 version = "0.4"
 # The full version, including alpha/beta/rc tags
-release = "0.4.0"
+release = "0.4.1"
 
 
 # -- General configuration ---------------------------------------------------
